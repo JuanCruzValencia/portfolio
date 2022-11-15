5b03e8b4b974a542edbb4640ee136280f574eaa8
JuanCruzValencia <jcvalencia@ismt.edu.ar>
2022-11-15 19:21:38 -0300

    new project added

--- projects/db.ts
+++ projects/db.ts
@@ -1,5 +1,15 @@
 export const projects = [
   {
+    title: "Aerolab Shop - e-commerce React App Project",
+    description:
+      "Aerolab SHop is an e-commerce web application that allows users to filter and redeem products from Aerolab API.",
+    url: "https://github.com/JuanCruzValencia/aerolab-challenge",
+    image:
+      "https://github.com/JuanCruzValencia/aerolab-challenge/blob/main/aerolab-challenge.gif",
+    techs: ["React", "Next", "ChakraUI", "AerolabAPI"],
+    deploy: "https://juancruzvalencia.github.io/aerolab-challenge/",
+  },
+  {
     title: "Baltos Shop - E-commerce React App Project",
     description:
       "Baltos Shop is a e-commerce web application that allows users to search for products stored in Firestore, add them to shopping cart and then make payment. App has login system functionality. The guest user is able to browse and add product to cart only. Checkout and payment option is available for registerd users.",
@@ -11,7 +21,8 @@ export const projects = [
   },
   {
     title: "JS News - News Portal Javascript App Project",
-    description: "The purpose of the the project is to create a web app for javascript couse in CoderHouse. For build the front-end functionality Vanilla JavaScript is used. Inside the project we can filter news, search for particular city forecast, write, update and delete notes, and do simple math in the calculator.",
+    description:
+      "The purpose of the the project is to create a web app for javascript couse in CoderHouse. For build the front-end functionality Vanilla JavaScript is used. Inside the project we can filter news, search for particular city forecast, write, update and delete notes, and do simple math in the calculator.",
     url: "https://github.com/JuanCruzValencia/News-web-app",
     image:
       "https://firebasestorage.googleapis.com/v0/b/my-portfolio-jcvv.appspot.com/o/screen-recording%20(2).gif?alt=media&token=ca17c812-c7fa-4a8d-a352-92de43d2b6cc",
